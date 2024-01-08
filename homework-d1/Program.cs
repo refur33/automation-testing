1c3fa82e7695a90a27d398e7b7bfc1b703ebaa42
refur33 <anna.tyletskaya@gmail.com>
2024-01-08 17:34:48 +0100

    update 7, 8

--- homework-d1/Program.cs
+++ homework-d1/Program.cs
@@ -1,24 +1,43 @@
-﻿int[] createAndFill()
+﻿int[] createAndFill(int n)
 // создать и заполнить массив, вывести в консоль и передать
 {
-    int[] myArray = new int[9];
+    int[] myArray = new int[n];
     Random rnd = new Random();
     for (int i = 0; i < myArray.Length; i++)
     {
         myArray[i] = rnd.Next(50);
     }
 
+    return myArray;
+};
+
+int[] createAndFillWithRandom(int n, int start, int end)
+// создать и заполнить массив, вывести в консоль и передать + старт и конец широты рандома
+{
+    int[] myArray = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        myArray[i] = rnd.Next(50);
+    }
+
+    return myArray;
+};
+
+void outputMyArray(int[] myArray) // отдельно для вывода в консоль
+{
+    
     for (int i = 0; i < myArray.Length; i++)
     {
         Console.Write(myArray[i] + ", ");
     }
-    return myArray;
 };
 
 void task1() // done
 // Найти минимальный элемент массива
 {
-    int[] myArray = createAndFill();
+    int[] myArray = createAndFill(10);
+    outputMyArray(myArray);
 
     int min = myArray.Min();
     Console.WriteLine("\n" + min);
@@ -28,7 +47,8 @@ void task1() // done
 void task2() // done
 // Найти максимальный элемент массива
 {
-    int[] myArray = createAndFill();
+    int[] myArray = createAndFill(50);
+    outputMyArray(myArray);
 
     int max = myArray.Max();
     Console.WriteLine("\n" + max);
@@ -38,7 +58,8 @@ void task2() // done
 void task3() // done
 // Найти индекс минимального элемента массива
 {
-    int[] myArray = createAndFill();
+    int[] myArray = createAndFill(20);
+    outputMyArray(myArray);
 
     int min = myArray.Min();
     int minIndex = Array.IndexOf(myArray, min);
@@ -49,7 +70,8 @@ void task3() // done
 void task4() // done
 // Найти индекс максимального элемента массива
 {
-    int[] myArray = createAndFill();
+    int[] myArray = createAndFill(20);
+    outputMyArray(myArray);
 
     int max = myArray.Max();
     int maxIndex = Array.IndexOf(myArray, max);
@@ -60,7 +82,8 @@ void task4() // done
 void task5() // done
 // Посчитать сумму элементов массива с нечетными индексами
 {
-    int[] myArray = createAndFill();
+    int[] myArray = createAndFill(30);
+    outputMyArray(myArray);
 
     int sum = 0;
     for (int i = 1; i < myArray.Length; i +=2)
@@ -74,7 +97,8 @@ void task5() // done
 void task6() // done
 // Сделать реверс массива (массив в обратном направлении)
 {
-    int[] myArray = createAndFill();
+    int[] myArray = createAndFill(40);
+    outputMyArray(myArray);
 
     Array.Reverse(myArray);
     Console.WriteLine();
@@ -86,13 +110,15 @@ void task6() // done
     Console.ReadLine(); // чтобы консоль не закрывалась
 };
 
-void task7() // in progress (крешается)
+void task7() // not ready yet
 // Посчитать количество нечетных элементов массива
+
 {
-    int[] myArray = createAndFill();
-    int index = 0;
+    int[] myArray = createAndFillWithRandom(10, 20, 30);
+    outputMyArray(myArray);
+
     int oddsNumber = 0;
-    for (index = 0; index > Array.MaxLength; index++)
+    for (int index = 0; index < myArray.Length; index++)
     {
         if (myArray[index] % 2 != 0)
         {
@@ -105,4 +131,25 @@ void task7() // in progress (крешается)
 
 };
 
-task7();
+void task8() // done
+// Поменять местами первую и вторую половину массива, например, для
+//массива 1 2 3 4, результат 3 4 1 2, или для 12345 - 45312
+
+{
+    int[] myArray = createAndFill(7);
+    outputMyArray(myArray);
+
+    int mid = myArray.Length / 2;
+    for (int i = 0; i < mid; i++)
+    {
+        int tmp = myArray[i];
+        myArray[i] = myArray[i + mid + (myArray.Length % 2)];
+        myArray[i + mid + (myArray.Length % 2)] = tmp;
+    }
+
+
+    Console.WriteLine(" ");
+    Console.WriteLine(string.Join(", ", myArray));
+    Console.ReadLine(); // чтобы консоль не закрывалась
+}
+
